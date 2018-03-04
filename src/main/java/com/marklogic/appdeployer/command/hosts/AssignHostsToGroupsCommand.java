dfdeffe2cb2757f3d634369309eb1391b3e1cc3f
Phil Barber <Phil.Barber@marklogic.com>
2018-03-04 12:34:15 -0500

    Issue-293 - This works. However, there are debug statements and it may not be perfect yet.

--- src/main/java/com/marklogic/appdeployer/command/hosts/AssignHostsToGroupsCommand.java
+++ src/main/java/com/marklogic/appdeployer/command/hosts/AssignHostsToGroupsCommand.java
@@ -1,8 +1,13 @@
 package com.marklogic.appdeployer.command.hosts;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+
 import com.marklogic.appdeployer.command.AbstractUndoableCommand;
 import com.marklogic.appdeployer.command.CommandContext;
 import com.marklogic.appdeployer.command.SortOrderConstants;
+import com.marklogic.mgmt.resource.hosts.HostManager;
 
 public class AssignHostsToGroupsCommand extends AbstractUndoableCommand {
 
@@ -13,8 +18,16 @@ public class AssignHostsToGroupsCommand extends AbstractUndoableCommand {
 
 	@Override
 	public void execute(CommandContext context) {
-		System.out.println("AAAAA");
+		System.out.println("Executing AssignHostsToGroupsCommand");
+		Map<String, String> hostGroups = context.getAppConfig().getHostGroups();
+		HostManager mgr = new HostManager(context.getManageClient());
+		for (Map.Entry<String, String> entry : hostGroups.entrySet()) {
+			System.out.println(format("%s -> %s", entry.getKey(), entry.getValue()));
+			ResponseEntity<String> response = mgr.setHostToGroup(entry.getKey(), entry.getValue());
+			System.out.println("Status Code: " + response.getStatusCode());
+		}
 	}
+
 
 	@Override
 	public void undo(CommandContext context) {
