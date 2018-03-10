50018de5492709e8f8a70efe433132a6bb5a6d6d
Phil Barber <Phil.Barber@marklogic.com>
2018-03-10 09:59:34 -0500

    Issue 293 - Ensure an App-Services server is created in the target group, and ensure the Admin server has the rewriter set correctly.

--- src/main/java/com/marklogic/appdeployer/command/hosts/AssignHostsToGroupsCommand.java
+++ src/main/java/com/marklogic/appdeployer/command/hosts/AssignHostsToGroupsCommand.java
@@ -12,9 +12,13 @@ import com.marklogic.mgmt.resource.hosts.HostManager;
 public class AssignHostsToGroupsCommand extends AbstractUndoableCommand {
 
 	private static final String MANAGE_SERVER_NAME = "Manage";
-	private static final String MANAGE_SERVER_JSON_TEMPLATE = "{\"server-name\":\"%s\", \"group-name\":\"%s\", \"server-type\":\"http\", \"enabled\":true, \"root\":\"Apps/\", \"authentication\":\"digest\", \"port\":8002, \"webDAV\":false, \"execute\":true, \"display-last-login\":false, \"address\":\"0.0.0.0\", \"backlog\":512, \"threads\":32, \"request-timeout\":30, \"keep-alive-timeout\":5, \"session-timeout\":3600, \"max-time-limit\":3600, \"default-time-limit\":600, \"max-inference-size\":500, \"default-inference-size\":100, \"static-expires\":3600, \"pre-commit-trigger-depth\":1000, \"pre-commit-trigger-limit\":10000, \"collation\":\"http://marklogic.com/collation/\", \"internal-security\":true, \"concurrent-request-limit\":0, \"compute-content-length\":true, \"log-errors\":false, \"debug-allow\":true, \"profile-allow\":true, \"default-xquery-version\":\"1.0-ml\", \"multi-version-concurrency-control\":\"contemporaneous\", \"distribute-timestamps\":\"fast\", \"output-sgml-character-entities\":\"none\", \"output-encoding\":\"UTF-8\", \"output-method\":\"default\", \"output-byte-order-mark\":\"default\", \"output-cdata-section-namespace-uri\":\"\", \"output-cdata-section-localname\":null, \"output-doctype-public\":\"\", \"output-doctype-system\":\"\", \"output-escape-uri-attributes\":\"default\", \"output-include-content-type\":\"default\", \"output-indent\":\"default\", \"output-indent-untyped\":\"default\", \"output-indent-tabs\":\"default\", \"output-media-type\":\"\", \"output-normalization-form\":\"none\", \"output-omit-xml-declaration\":\"default\", \"output-standalone\":\"omit\", \"output-undeclare-prefixes\":\"default\", \"output-version\":\"\", \"output-include-default-attributes\":\"default\", \"default-error-format\":\"compatible\", \"error-handler\":\"manage/error-handler.xqy\", \"url-rewriter\":\"manage/rewriter.xqy\", \"rewrite-resolves-globally\":false, \"ssl-allow-sslv3\":true, \"ssl-allow-tls\":true, \"ssl-disable-sslv3\":false, \"ssl-disable-tlsv1\":false, \"ssl-disable-tlsv1-1\":false, \"ssl-disable-tlsv1-2\":false, \"ssl-hostname\":\"\", \"ssl-ciphers\":\"ALL:!LOW:@STRENGTH\", \"ssl-require-client-certificate\":true, \"content-database\":\"App-Services\", \"default-user\":\"nobody\", \"privilege\":\"http://marklogic.com/xdmp/privileges/manage\"}";
-	
-    public AssignHostsToGroupsCommand() {
+	private static final String MANAGE_SERVER_JSON_TEMPLATE = "{\"server-name\":\"%s\", \"group-name\":\"%s\", \"server-type\":\"http\", \"root\":\"Apps/\", \"port\":8002, \"content-database\":\"App-Services\", \"error-handler\":\"manage/error-handler.xqy\", \"url-rewriter\":\"manage/rewriter.xqy\", \"privilege\":\"http://marklogic.com/xdmp/privileges/manage\"}";
+	private static final String APP_SERVER_NAME = "App-Services";
+	private static final String APP_SERVER_JSON_TEMPLATE = "{\"server-name\":\"%s\", \"group-name\":\"%s\", \"server-type\":\"http\", \"root\":\"/\", \"port\":8000, \"modules-database\":\"Modules\", \"content-database\":\"Documents\", \"error-handler\":\"/MarkLogic/rest-api/8000-error-handler.xqy\", \"url-rewriter\":\"/MarkLogic/rest-api/8000-rewriter.xml\", \"rewrite-resolves-globally\":true}";
+	private static final String ADMIN_SERVER_NAME = "Admin";
+	private static final String ADMIN_SERVER_UPDATE_JSON_TEMPLATE = "{\"server-name\":\"%s\", \"url-rewriter\":\"rewriter.xqy\"}";
+
+	public AssignHostsToGroupsCommand() {
         setExecuteSortOrder(SortOrderConstants.ASSIGN_HOSTS_TO_GROUPS);
         setUndoSortOrder(SortOrderConstants.UNASSIGN_HOSTS_FROM_GROUPS);
     }
@@ -22,12 +26,12 @@ public class AssignHostsToGroupsCommand extends AbstractUndoableCommand {
 	@Override
 	public void execute(CommandContext context) {
 		Map<String, String> hostGroups = context.getAppConfig().getHostGroups();
-	    AdminManager adminManager = context.getAdminManager();
 		HostManager mgr = new HostManager(context.getManageClient());
 		for (Map.Entry<String, String> entry : hostGroups.entrySet()) {
 			String hostName = entry.getKey();
 			String groupName = entry.getValue();
-			// First, ensure a Manage server exists for the group
+			// First, ensure a Manage and App-Services servers exists for the group
+			// This issue is fixed in ML 9.0-3 (https://bugtrack.marklogic.com/46909)
 			ServerManager serverMgr = new ServerManager(context.getManageClient(), groupName);
 			try {
 				String serverResponse = serverMgr.getPropertiesAsJson(MANAGE_SERVER_NAME, "group-id="+groupName);
@@ -41,8 +45,25 @@ public class AssignHostsToGroupsCommand extends AbstractUndoableCommand {
 	        		throw e;
 	        	}
 	        }
+			try {
+				String serverResponse = serverMgr.getPropertiesAsJson(APP_SERVER_NAME, "group-id="+groupName);
+				System.out.println("serverResponse: " + serverResponse);
+	        } catch (Exception e) {
+	        	if (e.getMessage().contains("404 Not Found")) {
+	                String appServicesServerPayload = format(APP_SERVER_JSON_TEMPLATE, APP_SERVER_NAME, groupName);
+	        		serverMgr.save(appServicesServerPayload);
+	        	} else {
+	        		logger.error("Error checking for App-Services server in target group: " + e.getMessage());
+	        		throw e;
+	        	}
+	        }
+			// When new groups are created, an Admin server is automatically created in that group.
+			// However, the Admin server's rewrite property is empty - causing problems with reading the timestamp
+            String adminServerPayload = format(ADMIN_SERVER_UPDATE_JSON_TEMPLATE, ADMIN_SERVER_NAME, groupName);
+			serverMgr.save(adminServerPayload);
+
 			mgr.setHostToGroup(hostName, groupName);
-			adminManager.waitForRestart();
+            context.getAdminManager().waitForRestart();
 		}
 	}
 
