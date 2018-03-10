3012485f2b30d25675537bbccf6e620b0e82281a
Phil Barber <Phil.Barber@marklogic.com>
2018-03-10 12:31:48 -0500

    Issue-293 - Refactored to use invokeActionRequiringRestart.

--- src/main/java/com/marklogic/appdeployer/command/hosts/AssignHostsToGroupsCommand.java
+++ src/main/java/com/marklogic/appdeployer/command/hosts/AssignHostsToGroupsCommand.java
@@ -5,12 +5,13 @@ import java.util.Map;
 import com.marklogic.appdeployer.command.AbstractUndoableCommand;
 import com.marklogic.appdeployer.command.CommandContext;
 import com.marklogic.appdeployer.command.SortOrderConstants;
-import com.marklogic.mgmt.admin.AdminManager;
+import com.marklogic.mgmt.admin.ActionRequiringRestart;
 import com.marklogic.mgmt.resource.appservers.ServerManager;
 import com.marklogic.mgmt.resource.hosts.HostManager;
 
 public class AssignHostsToGroupsCommand extends AbstractUndoableCommand {
 
+	private static final String DEFAULT_GROUP_NAME = "Default";
 	private static final String MANAGE_SERVER_NAME = "Manage";
 	private static final String MANAGE_SERVER_JSON_TEMPLATE = "{\"server-name\":\"%s\", \"group-name\":\"%s\", \"server-type\":\"http\", \"root\":\"Apps/\", \"port\":8002, \"content-database\":\"App-Services\", \"error-handler\":\"manage/error-handler.xqy\", \"url-rewriter\":\"manage/rewriter.xqy\", \"privilege\":\"http://marklogic.com/xdmp/privileges/manage\"}";
 	private static final String APP_SERVER_NAME = "App-Services";
@@ -25,8 +26,19 @@ public class AssignHostsToGroupsCommand extends AbstractUndoableCommand {
 
 	@Override
 	public void execute(CommandContext context) {
+		context.getAdminManager().invokeActionRequiringRestart(new ActionRequiringRestart() {
+			@Override
+			public boolean execute() {
+				return assignHostsToGroups(context);
+			}
+		});
+	}
+	
+	protected boolean assignHostsToGroups(CommandContext context) {
+		boolean requiresRestart = false;
+		
 		Map<String, String> hostGroups = context.getAppConfig().getHostGroups();
-		HostManager mgr = new HostManager(context.getManageClient());
+		HostManager hostMgr = new HostManager(context.getManageClient());
 		for (Map.Entry<String, String> entry : hostGroups.entrySet()) {
 			String hostName = entry.getKey();
 			String groupName = entry.getValue();
@@ -50,23 +62,35 @@ public class AssignHostsToGroupsCommand extends AbstractUndoableCommand {
 
 			// When new groups are created, an Admin server is automatically created in that group.
 			// However, the Admin server's rewrite property is empty - causing problems with reading the timestamp
+			// This fix should probably happen when the group is created.
             String adminServerPayload = format(ADMIN_SERVER_UPDATE_JSON_TEMPLATE, ADMIN_SERVER_NAME, groupName);
 			serverMgr.save(adminServerPayload);
-
-			mgr.setHostToGroup(hostName, groupName);
-            context.getAdminManager().waitForRestart();
+			
+			hostMgr.setHostToGroup(hostName, groupName);
+			requiresRestart = true;
 		}
+		return requiresRestart;
 	}
 
 
 	@Override
 	public void undo(CommandContext context) {
+		context.getAdminManager().invokeActionRequiringRestart(new ActionRequiringRestart() {
+			@Override
+			public boolean execute() {
+				return assignHostsToDefault(context);
+			}
+		});
+	}
+	
+	protected boolean assignHostsToDefault(CommandContext context) {
+		boolean requiresRestart = false;
 		Map<String, String> hostGroups = context.getAppConfig().getHostGroups();
-	    AdminManager adminManager = context.getAdminManager();
-		HostManager mgr = new HostManager(context.getManageClient());
+		HostManager hostMgr = new HostManager(context.getManageClient());
 		for (Map.Entry<String, String> entry : hostGroups.entrySet()) {
-			mgr.setHostToGroup(entry.getKey(), "Default");
-			adminManager.waitForRestart();
+			hostMgr.setHostToGroup(entry.getKey(), DEFAULT_GROUP_NAME);
+			requiresRestart = true;
 		}
+		return requiresRestart;
 	}
 }
