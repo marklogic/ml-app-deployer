34ab6fb5c6b6f17e9f5536461c791ed6ce9854ce
Steven Brockman <Steven.Brockman@marklogic.com>
2018-06-14 12:18:37 -0400

    Adding support for inserting host certificates

---
+++ src/main/java/com/marklogic/appdeployer/command/security/InsertCertificateHostsTemplateCommand.java
@@ -0,0 +1,190 @@
+package com.marklogic.appdeployer.command.security;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.marklogic.appdeployer.ConfigDir;
+import com.marklogic.appdeployer.command.AbstractResourceCommand;
+import com.marklogic.appdeployer.command.CommandContext;
+import com.marklogic.appdeployer.command.SortOrderConstants;
+import com.marklogic.mgmt.PayloadParser;
+import com.marklogic.mgmt.resource.ResourceManager;
+import com.marklogic.mgmt.resource.security.CertificateTemplateManager;
+
+import org.springframework.http.ResponseEntity;
+
+/**
+ * Used to insert (externally signed) host certificates for a template... by calling POST manage/v2/certificate-templates/[id-or-name] service. 
+ *    Place PEM formatted files under:  ml-config/security/certificate-templates/hostcertificates/YOUR-TEMPLATE
+ * 
+ * PEM Files MUST end with .crt AND .key
+ * 
+ * This command only inserts certificate hosts for templates that are defined in certificate-templates directory
+ * 
+ * Payload may contain operations as listed at:
+ * http://docs.marklogic.com/REST/POST/manage/v2/certificate-templates/[id-or-name]#getCFT
+ * <pre>
+ * Ex:
+ * {
+      "operation": "get-certificates-for-template"
+	 }
+	 </pre>
+ */
+public class InsertCertificateHostsTemplateCommand extends AbstractResourceCommand {
+	/**
+	 * The public certificate file to process
+	 */
+	private File publicCertFile;
+	/**
+	 * The private certificate key to match the above public cert
+	 */
+	private File privateCertFile;
+
+	/**
+	 * The name of the template that is being processed.
+	 */
+	private String superTemplateName;
+
+
+	public InsertCertificateHostsTemplateCommand() {
+		setExecuteSortOrder(SortOrderConstants.INSERT_CERTIFICATE_HOSTS_OPERATIONS);
+	}
+
+	public InsertCertificateHostsTemplateCommand(String superTemplateName) {
+		this();
+		this.superTemplateName = superTemplateName;
+	}
+
+	@Override
+	public String toString() {
+		if (publicCertFile != null) {
+			return publicCertFile.getAbsolutePath();
+		}
+		return this.superTemplateName;
+	}
+
+	/**
+	 * This execute method supports two primary modes:
+	 *  - Build Commands to insert the hosts
+	 * 	- IF a templateName was specified when constructing the Command, will process only that template
+	 *  - Process a single host certificate/key pair (publicCertFile)
+	 */
+	@Override
+	public void execute(CommandContext context) {
+		if (this.publicCertFile != null) {
+			// implies private key exists
+			insertHostCertificate(context);
+		} else {
+			// PULL template names to search operations directory
+			for (File resourceDir : getResourceDirs(context)) {
+				List<String> subTemplateNames = getTemplateNamesFromResourceDir(context, resourceDir);
+
+				for (String tn: subTemplateNames) {
+					// Check to only process template ID specified (if Command was constructed using Constructor)
+					if (superTemplateName == null || tn.equalsIgnoreCase(superTemplateName)) {
+						processHostCertificatesDir(context, tn);
+					}
+				}
+			}
+		}
+	}
+
+	protected List<String> getTemplateNamesFromResourceDir(CommandContext context, File resourceDir) {
+		List<String> subTemplateNames = new ArrayList<String>();
+		if (resourceDir.exists()) {
+			if (logger.isInfoEnabled()) {
+				logger.info("Using Template Names from directory: " + resourceDir.getAbsolutePath());
+			}
+			for (File f : listFilesInDirectory(resourceDir, context)) {
+				PayloadParser pp = new PayloadParser();
+				subTemplateNames.add(pp.getPayloadFieldValue(copyFileToString(f), "template-name"));
+			}
+		} else {
+			logResourceDirectoryNotFound(resourceDir);
+		}
+		return subTemplateNames;
+	}
+
+	public void insertHostCertificate(CommandContext context) {
+		CertificateTemplateManager certMgr = (CertificateTemplateManager) getResourceManager(context);
+		// Only insert host if a certificate does not exist
+		if (!certMgr.certificateExists(this.getSuperTemplateName())) {
+			String pubCertString = copyFileToString(this.publicCertFile);
+			String privateKeyString = copyFileToString(this.privateCertFile);
+
+			ResponseEntity<String> response = certMgr.insertHostCertificate(this.getSuperTemplateName(), pubCertString, privateKeyString);
+			if (logger.isDebugEnabled()) {
+				logger.debug("Response: " + response);
+			}
+		}
+	}
+
+    /**
+     * Processes all CRT files in the template directory
+     * NOTE: hostnames MUST end in crt and key.   Recommend:  myhost.marklogic.com.pem and myhost.marklogic.com.key
+	  * 
+     * @param context
+     * @param superTemplateName Name of the template the sub-operations are to be associated with
+     */
+    protected void processHostCertificatesDir(CommandContext context, String superTemplateName) {
+		for (ConfigDir configDir : context.getAppConfig().getConfigDirs()) {
+			File hostCertDir = new File(configDir.getCertificateTemplatesDir() + File.separator + "hostcertificates" + File.separator + superTemplateName);
+			logger.info(format("Checking for host certificates in: %s", hostCertDir.getAbsolutePath()));
+
+			if(hostCertDir.exists()){
+				for (File f : hostCertDir.listFiles()) {
+					logger.info("Found file: " + f.getAbsolutePath());
+					// Files must end in CRT
+					if (f.getName().endsWith("crt")) {
+						String keyFileString = f.getAbsolutePath().replace(".crt", ".key");
+						File pFile = new File(keyFileString);
+
+						if (pFile.exists()) {
+							logger.info("Found matching private key file: " + pFile.getAbsolutePath());
+							this.privateCertFile = f;
+							this.publicCertFile = pFile;
+
+							// Create COMMAND here
+							logger.info(format("About to insert host certificate for %s found in file: %s", superTemplateName, f.getAbsolutePath()));
+							InsertCertificateHostsTemplateCommand insertHostCommand = new InsertCertificateHostsTemplateCommand();
+							insertHostCommand.setPublicCertFile(f);
+							insertHostCommand.setPrivateCertFile(pFile);
+							insertHostCommand.setSuperTemplateName(superTemplateName);
+							insertHostCommand.execute(context);
+							logger.info(format("Inserting host certificate %s for template %s", f.getAbsolutePath(), superTemplateName));
+						}
+					}
+				}
+			}
+		}
+	 }
+	 
+	@Override
+	protected ResourceManager getResourceManager(CommandContext context) {
+		return new CertificateTemplateManager(context.getManageClient());
+	}
+
+	@Override
+	protected File[] getResourceDirs(CommandContext context) {
+		return findResourceDirs(context, configDir -> configDir.getCertificateTemplatesDir());
+	}
+
+	public void setSuperTemplateName(String name){
+		this.superTemplateName = name;
+	}
+
+	public String getSuperTemplateName() {
+		return this.superTemplateName;
+	}
+
+	public void setPublicCertFile(File publicCert) {
+		this.publicCertFile = publicCert;
+	}
+	public File getPublicCertFile() {
+		return this.publicCertFile;
+	}
+	public void setPrivateCertFile(File privateKey) {
+		this.privateCertFile = privateKey;
+	}
+}
--- src/main/java/com/marklogic/mgmt/resource/security/CertificateTemplateManager.java
+++ src/main/java/com/marklogic/mgmt/resource/security/CertificateTemplateManager.java
@@ -1,10 +1,12 @@
 package com.marklogic.mgmt.resource.security;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.marklogic.mgmt.ManageClient;
 import com.marklogic.mgmt.resource.AbstractResourceManager;
 import com.marklogic.mgmt.util.ObjectMapperFactory;
 import com.marklogic.rest.util.Fragment;
+
 import org.springframework.http.ResponseEntity;
 
 /**
@@ -72,7 +74,51 @@ public class CertificateTemplateManager extends AbstractResourceManager {
                     json));
         }
         return postPayload(getManageClient(), getResourcePath(templateIdOrName), json);
-    }
+	 }
+	
+	/**
+	 * Inserts a host certificate for a given template.
+	 * 
+	 * Even though service allows multiple inserts in one call, this only inserts one host cert at a time.  
+	 *   (As the command runs one at a time.)  Allows for discovery of bad/invalid certs
+	 * @param templateIdOrName The template name to insert the host certificates
+	 * @param pubCert the Public PEM formatted certificate
+	 * @param privateKey the Private PEM formatted certificate
+	 */
+	public ResponseEntity<String> insertHostCertificate(String templateIdOrName, String pubCert, String privateKey) {
+		ObjectNode command = ObjectMapperFactory.getObjectMapper().createObjectNode();
+		command.put("operation", "insert-host-certificates");
+		ArrayNode certs = ObjectMapperFactory.getObjectMapper().createArrayNode();
+		
+		ObjectNode certificate = ObjectMapperFactory.getObjectMapper().createObjectNode();
+		ObjectNode cert = ObjectMapperFactory.getObjectMapper().createObjectNode();
+
+		certificate.put("cert", pubCert);
+		certificate.put("pkey", privateKey);
+		cert.set("certificate", certificate);
+		certs.add(cert);
+
+		command.set("certificates", certs);
+
+		String json = command.toString();
+		if (logger.isInfoEnabled()) {
+			// NOTE - should NOT print out private key - EVER
+			logger.info(format("Inserting host certificate for template %s", templateIdOrName));
+		}
+		return postPayload(getManageClient(), getResourcePath(templateIdOrName), json);
+	}
+
+	/**
+	 * Utility service to determine if certificates exist for a template.
+	 * 
+	 * Used because ML9.0-5 (and prior) has bug for "needs-certificate" call
+	 */
+	public boolean certificateExists(String templateIdOrName) {
+		Fragment response = getCertificatesForTemplate(templateIdOrName);
+		logger.info(format("Checking if %s template has certificates --> for template: %s", templateIdOrName, response.getPrettyXml()));
+
+		return response.elementExists("/msec:certificate-list/msec:certificate");
+	}
 
     public Fragment getCertificatesForTemplate(String templateIdOrName) {
         ObjectNode node = ObjectMapperFactory.getObjectMapper().createObjectNode();
